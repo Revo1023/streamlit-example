cedd5a1f6d9519c1ddff0ff59db39b7bc32098e8
Revo1023 <154569471+Revo1023@users.noreply.github.com>
2024-02-23 17:05:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,16 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+latext = r'''
+## Latex example
+### full equation 
+$$ 
+\Delta G = \Delta\sigma \frac{a}{b} 
+$$ 
+### inline
+Assume $\frac{a}{b}=1$ and $\sigma=0$...  
+'''
+st.write(latext)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
